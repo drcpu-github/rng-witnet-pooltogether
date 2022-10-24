8e90dbcb9db9d6c17ef9e0bd200dddf4db638d6c
drcpu <drcpu@protonmail.com>
2022-10-24 20:29:02 +0200

    Split and specialize deploy functions for RngWitnet and WitnetRequestRandomness

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,29 @@
 import json
 
-from brownie import accounts, config, RngWitnet
+from brownie import accounts, config, RngWitnet, WitnetRequestRandomness
 
-def deploy_goerli():
-    deploy("goerli")
+def deploy_rng_witnet_goerli():
+    deploy_rng_witnet("goerli")
 
-def deploy(network):
+def deploy_rng_witnet(network):
     script_config = json.load(open("config.json"))
 
     accounts.add(config["wallets"]["from_key"])
     deploy_account = accounts[0]
 
-    RngWitnet.deploy(script_config[network]["witnet_request_board_address"], {"from": deploy_account}, publish_source=True)
+    assert network in script_config, "Network configuration not found"
+    assert script_config[network]["witnet_request_board_address"] != "", "Need to specify the address of the Witnet Request Board"
+    assert script_config[network]["witnet_request_randomness_address"] != "", "Need to specify the address of the Witnet Request Randomness"
+
+    RngWitnet.deploy(
+        script_config[network]["witnet_request_board_address"],
+        script_config[network]["witnet_request_randomness_address"],
+        {"from": deploy_account},
+        publish_source=True
+    )
+
+def deploy_witnet_request_randomness():
+    accounts.add(config["wallets"]["from_key"])
+    deploy_account = accounts[0]
+
+    WitnetRequestRandomness.deploy({"from": deploy_account}, publish_source=True)

9b780087bf39a69f4c4b99bfec1bbe0bde636ea9
drcpu <drcpu@protonmail.com>
2022-10-26 08:49:31 +0200

    Allow using a predefined RngWitnet deployment when requesting and fetching a random number

--- scripts/request_and_fetch_rng.py
+++ scripts/request_and_fetch_rng.py
@@ -1,20 +1,27 @@
+import json
 import time
 
-from brownie import accounts, config, RngWitnet
+from brownie import accounts, config, Contract, RngWitnet
 
 # This script serves as an example for the steps required to fetch a random number.
 # It only works if you add yourself as an allowed requester to the contract.
 
 def main():
+    script_config = json.load(open("config.json"))
+
     # Use the account defined in .env
     accounts.add(config["wallets"]["from_key"])
     my_account = accounts[0]
 
-    # Grab the latest deployment
-    rng_witnet = RngWitnet[-1]
+    # Grab an RngWitnet deployment
+    if script_config["rng_witnet_address"] != "":
+        abi = json.loads(open("build/contracts/RngWitnet.json").read())["abi"]
+        rng_witnet = Contract.from_abi("RngWitnet", script_config["rng_witnet_address"], abi)
+    else:
+        rng_witnet = RngWitnet[-1]
 
     # Request random number
-    txn = rng_witnet.requestRandomNumber({"from": my_account, "gas_limit": 400000})
+    txn = rng_witnet.requestRandomNumber({"from": my_account, "gas_limit": 300000})
     contract_request_id = txn.events["RngRequested"]["requestId"]
     witnet_request_id = txn.events["RngRequested"]["witnetRequestId"]
     print(f"Randomness request {contract_request_id} => {witnet_request_id}")

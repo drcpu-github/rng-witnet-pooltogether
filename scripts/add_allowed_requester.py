e7f06325b485fe15363606b8b706072ca21514c0
drcpu <drcpu@protonmail.com>
2022-10-26 08:50:18 +0200

    Allow using a predefined RngWitnet deployment when adding an allowed requester

--- scripts/add_allowed_requester.py
+++ scripts/add_allowed_requester.py
@@ -1,6 +1,6 @@
 import json
 
-from brownie import accounts, config, RngWitnet
+from brownie import accounts, config, Contract, RngWitnet
 
 def add_requesters_goerli():
     add_requesters("goerli")
@@ -12,8 +12,12 @@ def add_requesters(network):
     accounts.add(config["wallets"]["from_key"])
     my_account = accounts[0]
 
-    # Grab the latest deployment
-    rng_witnet = RngWitnet[-1]
+    # Grab an RngWitnet deployment
+    if script_config["rng_witnet_address"] != "":
+        abi = json.loads(open("build/contracts/RngWitnet.json").read())["abi"]
+        rng_witnet = Contract.from_abi("RngWitnet", script_config["rng_witnet_address"], abi)
+    else:
+        rng_witnet = RngWitnet[-1]
 
     for address in script_config[network]["prize_strategy_addresses"]:
         rng_witnet.addAllowedRequester(address, {"from": my_account})

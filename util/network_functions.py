797f2a618d91e92a2a5ed2cc5e4d71375fa2a93a
drcpu <drcpu@protonmail.com>
2022-11-01 21:05:46 +0100

    Add testnet and mainnet configurations

--- util/network_functions.py
+++ util/network_functions.py
@@ -1,23 +1,35 @@
 from brownie import accounts, config, network
 
-LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
+LOCAL_BLOCKCHAINS = [
     "ethereum-fork-infura",
     "ethereum-fork-alchemy",
     "goerli-fork-alchemy"
 ]
 
+TESTNET_BLOCKCHAINS = [
+    "goerli-alchemy",
+]
+
 def is_local_network():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if network.show_active() in LOCAL_BLOCKCHAINS:
+        return True
+    return False
+
+def is_testnet():
+    if network.show_active() in TESTNET_BLOCKCHAINS:
         return True
     return False
 
 def get_network():
     current_network = network.show_active()
-    return "-".join(current_network.split("-")[:2])
+    return "-".join(current_network.split("-")[:-1])
 
-def get_account(index):
+def get_account():
     if is_local_network():
-        return accounts[index]
+        return accounts[0]
     else:
-        accounts.add(config["wallets"]["from_key"])
-        return accounts[index]
+        if is_testnet():
+            accounts.add(config["wallets"]["from_key_goerli"])
+        else:
+            accounts.add(config["wallets"]["from_key_ethereum"])
+        return accounts[0]
